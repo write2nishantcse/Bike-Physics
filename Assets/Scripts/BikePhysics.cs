cd75e56eaf0ced12f6f6c488c8533690f5d5ec19
nishant kaushik <write2nishantcse@gmail.com>
2024-01-02 03:25:57 +0530

    Moving up the ramp + coming down

--- Assets/Scripts/BikePhysics.cs
+++ Assets/Scripts/BikePhysics.cs
@@ -11,6 +11,8 @@ public class BikePhysics : MonoBehaviour
     public Transform frontTyre;
     public Transform chasis;
     public Transform rearTyre;
+    public float distanceFromGround = 0.5f;
+    public float comingDownSpeed = 1;
 
     private Vector3 front_chasis_offset;
     private Vector3 chasis_rear_offset;
@@ -31,9 +33,38 @@ public class BikePhysics : MonoBehaviour
 
         // Lean based on turning
         transform.Rotate(Vector3.up, turnSpeed * horizontal);
-
         transform.position += transform.forward * vertical;
+
+        // check and move tyre
+        CheckTyrePositionWithGround();
+
+
         //InegrateBikeParts();
+    }
+
+    private void CheckTyrePositionWithGround()
+    {
+        if (Physics.Raycast(transform.position, transform.up * -1, out var castInfo, distanceFromGround + 1))
+        {
+            transform.position = castInfo.point + castInfo.normal * distanceFromGround;
+        }
+        else
+        {
+            transform.position += Vector3.down * comingDownSpeed * Time.deltaTime;
+        }
+    }
+
+    private bool IsTouchingGround()
+    {
+
+        if (Physics.Raycast(transform.position, transform.up * -1, out var castInfo))
+        {
+            if (castInfo.distance <= distanceFromGround)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void InegrateBikeParts()
